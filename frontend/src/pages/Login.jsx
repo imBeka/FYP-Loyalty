723bead7e3e1b5952b8e8d281f0fdf44df9f073b
imBeka <bka05yu@gmail.com>
2025-01-21 16:05:43 +0000

    Frontend: Authentification

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,38 @@
 import { useState, useEffect } from "react"
-import { FaSignInAlt, FaUser } from "react-icons/fa"
+import { FaSignInAlt } from "react-icons/fa"
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from "react-router-dom"
+import { toast } from 'react-toastify'
+import { login, reset } from "../features/auth/authSlice"
+import Spinner from "../components/Spinner"
 
 function Login() {
     const [formData, setFormData] = useState({
-        name: '',
         email: '',
         password: '',
-        password2: '',
     })
 
     const { email, password } = formData
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector(
+        (state) => state.auth
+    )
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+
+        if(isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
 
     const onChange = (e) => {    
         setFormData((prevState)=> ({
@@ -20,6 +43,12 @@ function Login() {
 
     const onSubmit = (e) => {
         e.preventDefault()
+
+        dispatch(login(formData))
+    }
+
+    if (isLoading) {
+        return <Spinner/>
     }
 
     return (      
